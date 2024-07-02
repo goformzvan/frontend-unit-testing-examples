9585d814b475fdcc8ca7ea145259fbc2bd6917b5
Van H <91505206+goformzvan@users.noreply.github.com>
2024-07-01 22:21:13 -0700

    More jest setup

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styles.ts',
   }
